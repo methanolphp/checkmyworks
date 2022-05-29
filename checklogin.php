d342f56fa95de6f565fc7ab3f29cfb1edab074f1
methanolphp <106457669+methanolphp@users.noreply.github.com>
2022-05-29 09:22:28 +0700

    Update checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -1,7 +1,7 @@
 <?php
 $checklogin = 0;
     $passcodingy=$_POST['passcode'];
-    if($passcodingy == "mcpscout6/2"){
+    if($passcodingy == ""){         # <-------put your passcode here
         $checklogin = 1;
         header("Location: home.php");
     }
